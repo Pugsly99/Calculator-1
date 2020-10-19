dfeb250d0c6917eea383f073e1b31e7dfd1ad0b0
Callie Stump <callie@stu.mp>
2020-10-19 13:27:08 -0400

    Adds readme, hmtl, and js to project

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+
+const number2 = parseInt(prompt("Enter a number:"));
+
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+alert(add(number1, number2));
+
